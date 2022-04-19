b713911c67269fe61c7ecfc38a20ffa34354bfa4
63A9F0EA7BB98050796B649E85481845 <info@rpdl.net>
2022-04-19 04:20:57 +0100

    added login modal to magnet button if not logged in

--- frontend/src/views/Torrent.vue
+++ frontend/src/views/Torrent.vue
@@ -34,7 +34,7 @@
                 <DownloadIcon class="justify-self-start mr-2 w-5 h-5"/>
                 <span>Torrent Download</span>
               </button>
-              <a type="button" :href="torrent.magnet_link"
+              <a type="button" @click="downloadMagnet"
                  class="mt-2 px-3 py-1.5 w-full flex flex-row justify-center text-sm text-white text-center bg-red-600 border border-red-600 rounded-md transition duration-200 hover:shadow-lg hover:shadow-red-600/25">
                 <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -180,6 +180,16 @@ export default {
         link.click();
       });
     },
+    downloadMagnet() {
+        if(!this.$store.getters.isLoggedIn){
+            this.$store.dispatch('openAuthModal');
+            return;
+    }
+    const link = document.createElement('a');
+    link.href = this.torrent.magnet_link;
+    document.body.appendChild(link);
+    link.click();
+},
     banUser(user) {
       const self = this;
       HttpService.delete(`/user/ban/${user}`, {}, () => {
