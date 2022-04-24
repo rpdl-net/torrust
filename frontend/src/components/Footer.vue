ddf68727d9e541b42b4673e878009099b9df64f8
63A9F0EA7BB98050796B649E85481845 <info@rpdl.net>
2022-04-24 17:19:06 +0100

    cleaned up footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -5,8 +5,7 @@
 <!--        <a class="hover:text-slate-400" href="">Terms of Service</a>-->
 <!--        <p class="sm:ml-4 sm:pl-4 sm:border-l sm:border-slate-200/5"><a class="hover:text-slate-400" href="">Privacy Policy</a></p>-->
 <!--      </div>-->
-      <a class="hover:text-white transition duration-200" href="https://rpdl.net/faq">FAQ</a>
-      <a class="ml-2 pl-2 border-l border-slate-600 hover:text-white transition duration-200" href="https://rpdl.net/donate">Donate</a><br>
+      <a class="hover:text-white transition duration-200" href="https://status.rpdl.net">Platform Status</a>
       <a class="ml-2 pl-2 border-l border-slate-600 hover:text-white transition duration-200" href="https://github.com/torrust/torrust">Powered by Torrust</a>
     </div>
   </div>
