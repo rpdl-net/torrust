68151d1ed817e51ee4aeedb2e9c92cedbab9c78a
rakema01 <rakema@duck.com>
2022-04-25 20:46:57 -0700

    More link experimentation

--- frontend/src/components/TorrentList.vue
+++ frontend/src/components/TorrentList.vue
@@ -42,7 +42,7 @@
 <!--                    <svg v-if="torrent.category_id === 1" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">-->
 <!--                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3" />-->
 <!--                    </svg>-->
-                    <span>{{ torrent.title }}</span>
+                    <a :href="`/torrent/${torrent.torrent_id.toString()}`">{{ torrent.title }}</a>
                   </div>
                 </td>
                 <td class="text-green-500 font-light">
