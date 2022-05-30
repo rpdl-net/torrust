0213168cc2aa0d599ceac6dca67784e9abaf8c1a
rakema01 <rakema@duck.com>
2022-05-30 00:58:52 -0700

    Scroll to top when changing pages

--- frontend/src/views/Torrents.vue
+++ frontend/src/views/Torrents.vue
@@ -8,7 +8,7 @@
       <FilterCategory />
       <ChangePageSize :update-page-size="updatePageSize" />
     </div>
-    <TorrentList class="mt-4" v-if="torrents.results.length > 0" :torrents="torrents.results" :sorting="sorting" :update-sorting="updateSorting"/>
+    <TorrentList id="tList" class="mt-4" v-if="torrents.results.length > 0" :torrents="torrents.results" :sorting="sorting" :update-sorting="updateSorting"/>
     <Pagination v-if="torrents.results.length > 0" :current-page.sync="currentPage" :total-pages="totalPages" :total-results="torrents.total" :page-size="pageSize" />
     <div v-else class="py-6 text-slate-400">This category has no results.</div>
   </div>
@@ -97,6 +97,7 @@ export default {
     },
     currentPage(newPage) {
       this.loadTorrents(newPage, this.sorting);
+      document.getElementById("tList").ScrollIntoView({behavior: "smooth"});
     },
     categoryFilters() {
       this.loadTorrents(this.currentPage, this.sorting);
