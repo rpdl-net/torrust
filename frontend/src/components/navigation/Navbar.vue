0b4c0bc3e0ccf887fcf90fa18ceb84e1ee1f7905
rakema01 <rakema@duck.com>
2022-06-01 18:59:14 -0700

    Shorten upload button

--- frontend/src/components/navigation/Navbar.vue
+++ frontend/src/components/navigation/Navbar.vue
@@ -9,7 +9,7 @@
           <div class="relative flex items-center ml-auto">
             <div v-if="$store.getters.isAdministrator" class="flex justify-between items-center space-x-4">
               <router-link to="/upload" class="px-4 py-1.5 bg-sky-500 text-sm text-white border border-sky-500 rounded-md transition duration-200 hover:shadow-lg hover:shadow-sky-500/25">
-                <span class="hidden md:block">Upload torrent</span>
+                <span class="hidden md:block">Upload</span>
                 <span class="block md:hidden">+</span>
               </router-link>
             </div>
