b05b7a85cc0c8341f7183b5a60549519cca599c7
rakema01 <rakema@duck.com>
2022-06-06 19:20:50 -0700

    Change donate button to gold

--- frontend/src/components/navigation/Navbar.vue
+++ frontend/src/components/navigation/Navbar.vue
@@ -14,7 +14,7 @@
               </router-link>
             </div>
             <div class="ml-4 flex items-center">
-              <a href="https://rpdl.net/donate/" class="px-4 py-1.5 bg-sky-500 text-sm text-white border border-sky-500 rounded-md transition duration-200 hover:shadow-lg hover:shadow-sky-500/25">
+              <a href="https://rpdl.net/donate/" class="px-4 py-1.5 bg-[#ffd700] text-sm text-[#0f172a] border border-[#ffd700] rounded-md transition duration-200 hover:shadow-lg hover:shadow-[#ffdf33]/25">
                 <span class="hidden md:block">Donate</span>
               </a>
             </div>
