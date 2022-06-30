08429b46a8d210ca0f9ea305e4989371c8d57048
rakema01 <rakema@duck.com>
2022-06-30 02:45:17 -0700

    Last fix for category filters for tonight

--- frontend/src/components/FilterCategory.vue
+++ frontend/src/components/FilterCategory.vue
@@ -56,6 +56,7 @@ export default {
       } else {
         filters.push(category);
       }
+      this.updateCategoryFilters(filters);
       this.$store.commit('setCategoryFilters', filters);
     },
     clearFilters() {
