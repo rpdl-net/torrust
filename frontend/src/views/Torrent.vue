94aa1d930bd34e614bf9f79617e9da11957882e5
63A9F0EA7BB98050796B649E85481845 <info@rpdl.net>
2024-11-28 02:02:45 +0000

    fix wording to make it more clear

--- frontend/src/views/Torrent.vue
+++ frontend/src/views/Torrent.vue
@@ -64,7 +64,7 @@
       <div>
         <div v-if="isAdmin" class="py-3 border-b border-slate-200/5"></div>
         <div class="px-3 py-2 text-2xl text-white bg-red-600 rounded-md hover:shadow-lg hover:shadow-red-600/25 transition duration-200">
-        Ensure your version of 7zip is updated. Read more <u><a href="https://www.zerodayinitiative.com/advisories/ZDI-24-1532/" target="_blank">here</a></u>.<br>Download newest version at the 7zip website, <u><a href="https://7-zip.org/download.html" target="_blank">7-zip.org</a></u>.</div>
+        Important: If you use 7zip, You need to update to version 24.08. Read more <u><a href="https://www.zerodayinitiative.com/advisories/ZDI-24-1532/" target="_blank">here</a></u><br>You can download it from <u><a href="https://7-zip.org/download.html" target="_blank">7-zip.org</a></u></div>
       </div>
       <div>
 <!--        <div class="py-3 border-b border-slate-200/5"></div>-->
