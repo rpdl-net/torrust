391ca26699e70c9c78de60e2937c2f74975431b2
63A9F0EA7BB98050796B649E85481845 <info@rpdl.net>
2024-12-24 21:54:45 +0000

    update links on landing page to new urls

--- frontend/src/views/Welcome.vue
+++ frontend/src/views/Welcome.vue
@@ -3,9 +3,9 @@
     <h1 class="view-title text-white">Safely download Ren'Py games, FAST.</h1>
 
     If you're new here, welcome! Here's a crash course:<br>
-    <u><a href="https://rpdl.net/docs/what-is-bittorrrent/" target="_blank">What is bittorrent</a></u>?<br>
-    <u><a href="https://rpdl.net/docs/using-our-torrents/" target="_blank">How to use our torrents</a></u>.<br>
-    <u><a href="https://rpdl.net/docs/best-practices/" target="_blank">Best practices</a></u> and <u><a href="https://rpdl.net/docs/recommended-clients/" target="_blank">Recommended torrent clients</a></u>.<br>
+    <u><a href="https://rpdl.net/docs/guide/what-is-bittorrent/" target="_blank">What is bittorrent</a></u>?<br>
+    <u><a href="https://rpdl.net/docs/guide/using-our-torrents/" target="_blank">How to use our torrents</a></u>.<br>
+    <u><a href="https://rpdl.net/docs/guide/best-practices/" target="_blank">Best practices</a></u> and <u><a href="https://rpdl.net/docs/recommended-clients/" target="_blank">Recommended torrent clients</a></u>.<br>
     Here's our <u><a href="https://rpdl.net/docs/faq/" target="_blank">FAQ</a></u> and if you've questions or feedback, or just wanna chat - join <u><a href="https://rpdl.net/discord" target="_blank">our Discord</a></u>!
   </div>
 </template>
