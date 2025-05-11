4475d173191046858970722e6bab28ce59e923e8
sudisk <sudisk@online.fr>
2025-05-11 17:04:12 +0200

    - Fix token renewal management and add configuration parameters
    - Bump some crate versions for recent Rust versions.
    - Improve logging capability

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,17 @@
-use std::sync::Arc;
-use actix_web::{App, HttpServer, middleware, web};
+use actix_contrib_logger::middleware::Logger;
 use actix_cors::Cors;
-use torrust::database::Database;
-use torrust::{handlers};
-use torrust::config::{Configuration};
-use torrust::common::AppData;
+use actix_web::{web, App, HttpServer};
+use log::Level;
+use reqwest::StatusCode;
+use std::sync::Arc;
 use torrust::auth::AuthorizationService;
-use torrust::tracker::TrackerService;
+use torrust::common::AppData;
+use torrust::config::Configuration;
+use torrust::database::SqliteDatabase;
+use torrust::handlers;
+use torrust::handlers::user::{DbUserRepository, RegistrationService, Repository};
 use torrust::mailer::MailerService;
+use torrust::tracker::TrackerService;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -20,19 +24,28 @@ async fn main() -> std::io::Result<()> {
 
     let settings = cfg.settings.read().await;
 
-    let database = Arc::new(Database::new(&settings.database.connect_url).await);
+    let database = Arc::new(SqliteDatabase::new(&settings.database.connect_url).await);
     let auth = Arc::new(AuthorizationService::new(cfg.clone(), database.clone()));
     let tracker_service = Arc::new(TrackerService::new(cfg.clone(), database.clone()));
     let mailer_service = Arc::new(MailerService::new(cfg.clone()).await);
-    let app_data = Arc::new(
-        AppData::new(
-            cfg.clone(),
-            database.clone(),
-            auth.clone(),
-            tracker_service.clone(),
-            mailer_service.clone(),
-        )
-    );
+    let user_repository: Arc<Box<dyn Repository>> =
+        Arc::new(Box::new(DbUserRepository::new(database.clone())));
+
+    let registration_service = Arc::new(RegistrationService::new(
+        cfg.clone(),
+        mailer_service.clone(),
+        user_repository.clone(),
+        // user_profile_repository.clone(),
+    ));
+
+    let app_data = Arc::new(AppData::new(
+        cfg.clone(),
+        database.clone(),
+        auth.clone(),
+        tracker_service.clone(),
+        mailer_service.clone(),
+        registration_service.clone(),
+    ));
 
     // create/update database tables
     let _ = sqlx::migrate!().run(&database.pool).await;
@@ -62,16 +75,28 @@ async fn main() -> std::io::Result<()> {
 
     drop(settings);
 
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("error"));
+
     println!("Listening on 0.0.0.0:{}", port);
 
     HttpServer::new(move || {
+        let logger = Logger::default().custom_level(|status| {
+            if status.is_server_error() {
+                Level::Error
+            } else if status == StatusCode::NOT_FOUND {
+                Level::Warn
+            } else {
+                Level::Info
+            }
+        });
+
         App::new()
             .wrap(Cors::permissive())
             .app_data(web::Data::new(app_data.clone()))
-            .wrap(middleware::Logger::default())
+            .wrap(logger)
             .configure(handlers::init_routes)
     })
-        .bind(("0.0.0.0", port))?
-        .run()
-        .await
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
 }
